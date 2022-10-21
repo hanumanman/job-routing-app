228c505aa0cbeaa9cb0dedae5187c17c7b050553
Itztla <hanumanman37@gmail.com>
2022-10-21 10:44:08 +0700

    add details to job modal

--- src/routes/JobModal.jsx
+++ src/routes/JobModal.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import "./modal.css";
 import Typography from "@mui/material/Typography";
 import ThemeProvider from "../contexts/ThemeProvider";
+import { Chip } from "@mui/material";
 
 function JobModal() {
   const id = useParams().jobId;
@@ -14,7 +15,23 @@ function JobModal() {
     <ThemeProvider>
       <div id="modalDiv">
         <h1>{`${job.title}`}</h1>
-
+        <div>
+          {job.skills.map((skill) => (
+            <Chip
+              color="success"
+              size="small"
+              label={skill}
+              sx={{ margin: 1 }}
+              key={`${job.id}${skill}`}
+            />
+          ))}
+        </div>
+        <p>{job.description}</p>
+        <p>Experience required: {job.yrsXPExpected} years</p>
+        <Typography variant="h7">
+          Range: {job.salaryLow} - {job.salaryHigh}
+        </Typography>
+        <Typography variant="h7">Location: {job.city}</Typography>
         <Link to={"/"}>Back</Link>
       </div>
     </ThemeProvider>
--- src/routes/LoginModal.jsx
+++ src/routes/LoginModal.jsx
@@ -1,11 +1,12 @@
 import { Button, Stack, Typography } from "@mui/material";
 import React from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import { FormProvider, FTextField } from "../components/form";
 import useAuth from "../hooks/useAuth";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
+import "./modal.css";
 
 const LoginSchema = Yup.object().shape({
   username: Yup.string().required("Username is required"),
